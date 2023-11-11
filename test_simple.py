7f613cb8287335f2baecba8cbdbd78c32bfb4b82
arif <arif@themascoteers.com>
2023-11-11 12:11:33 +1100

    done

--- test_simple.py
+++ test_simple.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
         spec_utils.get_wavelengths()
     ]
     algorithms = ["ann_simple","ann_simple_10","ann_simple_sum"]
-    alphas = [0]
+    alphas = [0,0,0]
     c = Evaluator(prefix="test_simple", folds=10, algorithms=algorithms, column_groups=column_groups, alphas=alphas)
     c.process()
