a69066e7822f95d0e8841c8e7f49abf57ccf29a0
arif <arif@themascoteers.com>
2023-11-11 13:34:37 +1100

    done

--- ann_utils.py
+++ ann_utils.py
@@ -3,6 +3,7 @@ from ann_simple_10 import ANNSimple10
 from ann_simple_sum import ANNSimpleSum
 from ann_simple_10_sum import ANNSimple10Sum
 from ann_simple_100_sum import ANNSimple100Sum
+from ann_simple_1000_sum import ANNSimple1000Sum
 from ann_savi_rev import ANNSAVIRev
 
 
@@ -17,6 +18,8 @@ def get_ann_by_name(algorithm):
         return ANNSimple10Sum
     elif algorithm == "ann_simple_100_sum":
         return ANNSimple100Sum
+    elif algorithm == "ann_simple_1000_sum":
+        return ANNSimple1000Sum
     elif algorithm == "ann_savi_rev":
         return ANNSAVIRev
 
--- test_simple.py
+++ test_simple.py
@@ -5,9 +5,7 @@ if __name__ == "__main__":
     column_groups = [
         spec_utils.get_wavelengths()
     ]
-    algorithms = ["ann_simple","ann_simple_10","ann_simple_sum","ann_simple_10_sum"]
-    algorithms = ["ann_simple"]
-    alphas = [0,0,0,0]
-    alphas = [0]
+    algorithms = ["ann_simple_10_sum","ann_simple_100_sum","ann_simple_1000_sum"]
+    alphas = [0,0,0,]
     c = Evaluator(prefix="ann_simple_lr", folds=10, algorithms=algorithms, column_groups=column_groups, alphas=alphas)
     c.process()
