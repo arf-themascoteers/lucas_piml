d86ed28f4a75d0653084f114451c9009ab980a56
arif <arif@themascoteers.com>
2023-11-11 11:37:09 +1100

    done

--- ann_utils.py
+++ ann_utils.py
@@ -1,7 +1,10 @@
+from ann_simple import ANNSimple
 from ann_savi_rev import ANNSAVIRev
 
 
 def get_ann_by_name(algorithm):
+    if algorithm == "ann_simple":
+        return ANNSimple
     if algorithm == "ann_savi_rev":
         return ANNSAVIRev
 
---
+++ test_simple.py
@@ -0,0 +1,11 @@
+from evaluator import Evaluator
+import spec_utils
+
+if __name__ == "__main__":
+    column_groups = [
+        spec_utils.get_wavelengths()
+    ]
+    algorithms = ["ann_simple"]
+    alphas = [0]
+    c = Evaluator(prefix="test", folds=10, algorithms=algorithms, column_groups=column_groups, alphas=alphas)
+    c.process()
